a9de40b2288c5aa374d0e11f6eb817c6c08b3d17
Jffm <joao.francisco.doc@gmail.com>
2022-04-10 22:02:47 -0300

    primeiras aulas

---
+++ 02-Input_de_variaveis_e_formatacao_de_strings/teste.js
@@ -0,0 +1,5 @@
+var nw = Number.ParseFloat(window.prompt("add "))
+window.alert("a soma e " + s)
+var n1 = Number.ParseInt(window.prompt("fala um numero "))
+var s = n1 + n2
+window.alert("a soma e " + s)
